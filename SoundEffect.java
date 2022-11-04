4e99f34f1ac8f77137174a1501891f7e618b1c69
Hussen Dube <tuo85171@temple.edu>
2022-11-04 16:08:00 -0400

    commit 3 SE

---
+++ SoundEffect.java
@@ -0,0 +1,85 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+
+public class SoundEffect implements Runnable
+{
+    private boolean running = true;
+    private Thread thread;
+
+    public SoundEffect()
+    {
+        this.start();
+    }
+
+    public void start()
+    {
+        if(running)
+            return;
+        this.thread = new Thread(this);
+        this.running = true;
+        this.thread.start();
+    }
+
+    //
+    private boolean playSong = true;
+    private AudioInputStream inputStream;
+    private String url;
+    private Clip clip;
+
+    @Override
+    public void run()
+    {
+        while(running)
+        {
+            if(inputStream == null && playSong)
+            {
+                this.playSong = false;
+                try
+                {
+                    this.inputStream = AudioSystem.getAudioInputStream(Sound.class.getResource(url));
+                    this.clip.open(inputStream);
+                    this.clip.start();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void playBackGround(String string) // call to play .wav file
+    {
+        if(this.clip != null)
+        {
+            this.clip.stop();
+            this.clip.close();
+        }
+        try
+        {
+            this.clip = AudioSystem.getClip();
+        }
+        catch(LineUnavailableException e)
+        {
+            e.printStackTrace();
+        }
+        url = string;
+        this.playSong = true;
+        this.inputStream = null;
+    }
+
+    public void disposeSound()
+    {
+        if(this.clip != null)
+        {
+            this.clip.stop();
+            this.clip.close();
+        }
+        this.clip = null;
+        this.playSong = true;
+        this.inputStream = null;
+        this.run();
+    }
+}
