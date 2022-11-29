cf4cb2114b756a7ac364c6499405c2a0ea9096d1
123Eriya <miyata0xyz@gmail.com>
2022-11-29 02:08:46 -0500

    fixed sound bugs

--- BackgroundSound.java
+++ BackgroundSound.java
@@ -13,10 +13,10 @@ public class BackgroundSound {
     private Clip clip;
     private volatile boolean running,looping;
     public BackgroundSound(){
-        looping = running = false;
+        running = false;
         try {
             clip = AudioSystem.getClip();
-            clip.open(AudioSystem.getAudioInputStream(new File("res/sounds/mixkit-just-chill-16.wav")));
+            clip.open(AudioSystem.getAudioInputStream(BackgroundSound.class.getResource("res/sounds/mixkit-just-chill-16.wav")));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (LineUnavailableException ex) {
@@ -30,16 +30,31 @@ public class BackgroundSound {
 
     public void play() {
         running = true;
+        looping = true;
         clip.setFramePosition(0);
         new Thread(new Runnable(){
             public void run(){
-                clip.start();
-                while(true){
-                    if(clip.getMicrosecondPosition() == clip.getMicrosecondLength())
+
+                clip.loop(10);
+                while (true) {
+                    if (clip.getMicrosecondPosition() == clip.getMicrosecondLength())
                         break;
-                    if(!running)
+                    if (!running)
                         break;
                 }
+
+                try {
+                    clip = AudioSystem.getClip();
+                    clip.open(AudioSystem.getAudioInputStream(BackgroundSound.class.getResource("res/sounds/mixkit-just-chill-16.wav")));
+                } catch (LineUnavailableException e) {
+                    throw new RuntimeException(e);
+                } catch (UnsupportedAudioFileException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+
             }
         }).start();
     }
